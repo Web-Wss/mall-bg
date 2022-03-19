34315b1765e46b405420b5634a879c0314125a8e
爽爽同学 <1306822660@qq.com>
2022-03-19 17:30:26 +0800

    状态和操作

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -18,14 +18,56 @@
     </el-row>
 
     <!-- 表格 -->
-    <el-table :data="tableData" style="width: 100%">
+    <el-table :data="userlist" style="width: 100%">
       <el-table-column type="index" label="#" width="60"> </el-table-column>
-      <el-table-column prop="name" label="姓名" width="80"> </el-table-column>
-      <el-table-column prop="address" label="邮箱"> </el-table-column>
-      <el-table-column prop="address" label="电话"> </el-table-column>
-      <el-table-column prop="address" label="创建时间"> </el-table-column>
-      <el-table-column prop="address" label="用户状态"> </el-table-column>
-      <el-table-column prop="address" label="操作"> </el-table-column>
+      <el-table-column prop="username" label="姓名" width="80">
+      </el-table-column>
+      <el-table-column prop="email" label="邮箱"> </el-table-column>
+      <el-table-column prop="mobile" label="电话"> </el-table-column>
+      <!-- <el-table-column prop="create_time | fmtdate" label="创建时间">
+      </el-table-column> -->
+      <el-table-column label="创建时间">
+        <template slot-scope="scope">
+          {{ scope.row.create_time | fmtdate }}
+        </template>
+      </el-table-column>
+      <el-table-column label="用户状态">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.mg_state"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+          >
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column prop="address" label="操作">
+        <template>
+          <el-row>
+            <el-button
+              size="mini"
+              plain
+              type="primary"
+              icon="el-icon-edit"
+              circle
+            ></el-button>
+            <el-button
+              size="mini"
+              plain
+              type="danger"
+              icon="el-icon-delete"
+              circle
+            ></el-button>
+            <el-button
+              size="mini"
+              plain
+              type="success"
+              icon="el-icon-check"
+              circle
+            ></el-button>
+          </el-row>
+        </template>
+      </el-table-column>
     </el-table>
     <!-- 分页 -->
   </el-card>
@@ -36,31 +78,12 @@ export default {
   data() {
     return {
       query: "",
+      // 表格绑定的数据
+      userlist: [],
+      // 分页相关数据
       pagenum: 1,
       pagesize: 2,
-      // 表格绑定的数据
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
+      total: -1
     };
   },
   created() {
@@ -80,6 +103,21 @@ export default {
         `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`
       );
       console.log(res);
+      const {
+        meta: { status, msg },
+        data: { users, total }
+      } = res.data;
+      if (status === 200) {
+        // 给表格数据赋值
+        this.userlist = users;
+        // 给总数赋值
+        this.total = total;
+        // 提示
+        this.$message.success(msg);
+      } else {
+        // 提示
+        this.$message.warning(msg);
+      }
     }
   }
 };
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from "vue";
 import App from "./App";
 import ElementUI from "element-ui";
 import MyServerHttp from "@/plugins/http";
+import moment from "moment";
 // 不要忘记引入css文件
 import "element-ui/lib/theme-chalk/index.css";
 import "@/assets/css/reset.css";
@@ -13,6 +14,10 @@ import router from "./router";
 Vue.use(ElementUI);
 Vue.use(MyServerHttp);
 
+// 全局过滤器--处理日期
+Vue.filter("fmtdate", v => {
+  return moment(v).format("YYYY-MM-DD");
+});
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
